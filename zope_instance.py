0550bd83815d8522fa44d6fa12e567f4a71e35a6
yomguy <yomguy@457c0346-1240-4656-8a5a-9edca8063506>
2007-11-03 17:31:07 +0000

    Update licence
    git-svn-id: http://svn.parisson.org/svn/tools/trunk@4 457c0346-1240-4656-8a5a-9edca8063506

--- zope_instance.py
+++ zope_instance.py
@@ -6,6 +6,19 @@
 #
 # This software is licensed as described in the file COPYING, which
 # you should have received as part of this distribution.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # Author: Guillaume Pellerin <yomguy@parisson.com>
 
