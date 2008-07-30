b7fa17f607d41b7170c986e047150a2862558beb
yomguy <yomguy@457c0346-1240-4656-8a5a-9edca8063506>
2008-07-30 15:26:21 +0000

    Fix filenames
    git-svn-id: http://svn.parisson.org/svn/tools/trunk@47 457c0346-1240-4656-8a5a-9edca8063506

--- various/webthumb_list.py
+++ various/webthumb_list.py
@@ -7,15 +7,17 @@ import os
 import sys
 from webthumb import get_thumbnail
 
-img_dir = '/var/www/img/webthumbs'
-site_list_file = '/var/www/img/webthumbs/webthumb_list.txt'
+img_dir = '/var/www/files/img/webthumbs'
+site_list_file = '/var/www/files/img/webthumbs/webthumb_list.txt'
 
 def main(site_list_file, img_dir):
     site_list = open(site_list_file,'r')
     for site in site_list.readlines():
         site = site[0:len(site)-1]
         print site
-        get_thumbnail('http://'+site, img_dir+os.sep+site+'.png','large')
+	file = site.replace('/','_')
+	file = img_dir+os.sep+site+'.png'
+        get_thumbnail('http://'+site, file,'large')
         #command = 'webthumb http://'+ site +' | pnmscale -xysize 650 400 | ' + \
         #          'pnmtopng | convert -crop 510x275+5+60 - ' + img_dir + os.sep + site +'.png'
         #print command
