60083488755e8b20a02d549477866cdc206c680d
yomguy <yomguy@parisson.com>
2012-03-05 13:44:33 +0100

    add loop

--- various/make_ids.py
+++ various/make_ids.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python
 
-import sys
 import hashlib, time, base64
 
-def makeSessionId(st):
+def get_an_id(st):
     m = hashlib.md5()
     m.update('this is a test of the emergency broadcasting system')
     m.update(str(time.time()))
     m.update(str(st))
     return base64.encodestring(m.digest())[:-3].replace('/', 'a').replace('+','b')
 
-print makeSessionId(sys.argv[-1])
+N= 1000
+ids = []
+for i in range(0, N):
+    ids.append(get_an_id(str(i)))
+    
+print ids
 
