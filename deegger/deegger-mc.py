33bbcc691ed7e8d711b5741e55d8e8fabfc49441
Guillaume Pellerin <yomguy@parisson.com>
2014-09-30 02:50:22 +0200

    add MixCloud audio downloader

---
+++ deegger/deegger-mc.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+Copyright (C) 2014 Guillaume Pellerin <yomguy@parisson.com>
+
+"""
+
+
+import sys, urllib2, webbrowser
+from pyquery import PyQuery as pq
+
+
+class MixCloud(object):
+
+    base_url = 'https://stream21.mixcloud.com/c/m4a/64/'
+    block_size = 16384
+
+    def __init__(self, url):
+        self.url = url
+        self.pq = pq(url=self.url)
+        shift = 0
+        if self.url[-1] == '/':
+            shift = 1
+        self.id = url.split('/')[-1-shift]
+        self.user = url.split('/')[-2-shift]
+
+    def get_media_url(self):
+        div = self.pq('.cloudcast-waveform')
+        url = div.attr('m-waveform').split('/')[2:]
+        url[0] = self.base_url
+        url[-1] = url[-1].replace('json', 'm4a')
+        return '/'.join(url)
+
+    def download(self):
+        filename = self.user + '_-_' + self.id + '.m4a'
+        url = self.get_media_url()
+        u = urllib2.urlopen(url)
+        f = open(filename, 'wb')
+        meta = u.info()
+        file_size = int(meta.getheaders("Content-Length")[0])
+        print "Downloading: %s Bytes: %s" % (filename, file_size)
+        file_size_dl = 0
+        while True:
+            buffer = u.read(self.block_size)
+            if not buffer:
+                break
+            file_size_dl += len(buffer)
+            f.write(buffer)
+            status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+            status = status + chr(8)*(len(status)+1)
+            print status,
+        f.close()
+
+
+def main():
+    url = sys.argv[-1]
+    mc = MixCloud(url)
+    media_url = mc.get_media_url()
+    # webbrowser.open(media_url)
+    mc.download()
+
+if __name__ == '__main__':
+    main()
+
